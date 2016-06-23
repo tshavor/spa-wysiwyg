287cc6f1a1ffde1caf8b96e213e2adc246b5fb1d
Thomas J. Shavor <thomasshavor@gmail.com>
2016-06-23 08:50:24 -0500

    array created

---
+++ wysiwyg.js
@@ -0,0 +1,132 @@
+// Object example:
+{
+  title: "12 GREATEST PROGRAMMERS OF ALL TIME",
+  name: "Ada Lovelace",
+  bio: "Augusta Ada King, more commonly known as Ada Lovelace, was an English mathematician and the world’s first computer programmer and was chiefly known for her work on Charles Babbage’s early mechanical general-purpose computer, the Analytical Engine. The notes she created for the Analytical Engine include what is recognized as the very first algorithm created solely for the intent of being processed by a machine or in other words, the world’s first computer program.",
+  image: "http://www.arkhitech.com/wp-content/uploads/2015/04/ada-lovelace.jpg"
+  lifespan: {
+    birth: 1815,
+    death: 1852
+  }
+}
+
+{
+  title: "12 GREATEST PROGRAMMERS OF ALL TIME",
+  name: "Niklaus Wirth",
+  bio: "Niklaus Emil Wirth is a Swiss computer scientist who is regarded as a pioneer of computer programming among other fields in software engineering. He is best known for designing several programming languages, including the highly popular Pascal, Euler, Algol W, Modula, Modula-2, Oberon, Oberon-2, and Oberon-07. He also designed the simple programming language PL/0 to illustrate compiler design which formed the basis for many university compiler design classes.",
+  image: "http://www.arkhitech.com/wp-content/uploads/2015/04/Niklaus-Wirth.jpg"
+  lifespan: {
+    birth: 1934,
+    death: 
+  }
+}
+
+{
+  title: "12 GREATEST PROGRAMMERS OF ALL TIME",
+  name: "Bill Gates",
+  bio: "Arguably one of the most popular computer programmers of all time, Bill Gates is an American business magnate, computer programmer, PC pioneer, investor, and philanthropist. He is the co-founder, ex-executive officer and current chairman of Microsoft, which is the world’s largest personal-computer software company. He is the best-known entrepreneurs of the personal computer revolution and helped develop Windows, which is the most used operating system in the world.",
+  image: "http://www.arkhitech.com/wp-content/uploads/2015/04/Bill-Gates.jpg"
+  lifespan: {
+    birth: 1955,
+    death:
+  }
+}
+
+{
+  title: "12 GREATEST PROGRAMMERS OF ALL TIME",
+  name: "James Gosling",
+  bio: "James Arthur Gosling is a Canadian computer scientist and an officer of the order of Canada. He has coded quite a number of programs but is widely known for his creation of the highly successful and commonly used Java programming language in 1994 as well as its original virtual machine and compiler. He credits his creation towards his graduate student days where he created a p-code virtual machine for the lab’s DEC VAX computer, so that his professor could run UCSD Pascal programs and then realized that the architecture-neutral execution for widely distributed programs could be achieved by a similar technique.",
+  image: "http://www.arkhitech.com/wp-content/uploads/2015/04/james-gosling-java-inventor.jpg"
+  lifespan: {
+    birth: 1955,
+    death: 
+  }
+}
+
+{
+  title: "12 GREATEST PROGRAMMERS OF ALL TIME",
+  name: "Guido van Rossum",
+  bio: "Guido van Rossum is a Dutch computer programmer who is the author of the popular Python programming language that is wildly used today. His creation of Python lead him to being declared a “Benevolent Dictator For Life” the In the Python community which means that he continues to oversee the Python development process, making decisions where necessary, forever.",
+  image: "http://www.arkhitech.com/wp-content/uploads/2015/04/Guido-van-Rossum.jpg"
+  lifespan: {
+    birth: 1956,
+    death: 
+  }
+}
+
+{
+  title: "12 GREATEST PROGRAMMERS OF ALL TIME",
+  name: "Ken Thompson",
+  bio: "Kenneth Lane Thompson, or simply “Ken” as he is called by the hacker community, is an American pioneer of computer science. Thompson had designed and implemented the original Unix operating system and also invented the B programming language (the direct predecessor to the famous C programming language), and was one of the early developers of the Plan 9 operating systems.",
+  image: "http://www.arkhitech.com/wp-content/uploads/2015/04/Kenneth-Lane-Thompson.jpg"
+  lifespan: {
+    birth: 1943,
+    death: 
+  }
+}
+
+{
+  title: "12 GREATEST PROGRAMMERS OF ALL TIME",
+  name: "Donald Knuth",
+  bio: "Donald Ervin Knuth is an American computer scientist and mathematician as well as a Professor Emeritus (retired professor) at Stanford University. Knuth has been dubbed as the “Father of the Analysis of Algorithms” as he has contributed to the development of rigorous analysis of the computational complexity of algorithms and systematized formal mathematical techniques for it.",
+  image: "http://www.arkhitech.com/wp-content/uploads/2015/04/Donald-Knuth.jpg"
+  lifespan: {
+    birth: 1747,
+    death: 1797
+  }
+}
+
+{
+  title: "12 GREATEST PROGRAMMERS OF ALL TIME",
+  name: "Brian Kernighan",
+  bio: "Brian Wilson Kernighan is a Canadian computer scientist who worked at Bell Labs alongside Ken Thompson and Dennis Ritchie. He is the co-creator and developer of UNIX. He is also co-author of the AWK and AMPL programming languages. Kernighan is currently a Professor and the Undergraduate Department Representative at the Computer Science Department of Princeton University.",
+  image: "http://www.arkhitech.com/wp-content/uploads/2015/04/Brian-Kernighan.jpg"
+  lifespan: {
+    birth: 1942,
+    death: 
+  }
+}
+
+{
+  title: "12 GREATEST PROGRAMMERS OF ALL TIME",
+  name: "Tim Berners-Lee",
+  bio: "Sir Timothy John “Tim” Berners-Lee is a British computer scientist who is renowned all across the globe because of his creation of the World Wide Web as well as the implementation of the first successful communication between a Hypertext Transfer Protocol (HTTP) client and server via the Internet in November, 1989.",
+  image: "http://www.arkhitech.com/wp-content/uploads/2015/04/Tim-Berners-Lee.jpg"
+  lifespan: {
+    birth: 1955,
+    death: 
+}
+
+{
+  title: "12 GREATEST PROGRAMMERS OF ALL TIME",
+  name: "Bjarne Stroustrup",
+  bio: "Bjarne Stroustrup is a Danish computer scientist who is credited for the creation and the development of the widely used and highly successful C++ programming language. He not only invented it, but also evolved it, all by himself, by writing its early definitions, producing its first implementation, formulating its design criteria, designing all its major facilities, processing extension proposals for standards committee and its standard textbook.",
+  image: "http://www.arkhitech.com/wp-content/uploads/2015/04/Bjarne-Stroustrup.jpg"
+  lifespan: {
+    birth: 1950,
+    death: 
+  }
+}
+
+{
+ title: "12 GREATEST PROGRAMMERS OF ALL TIME",
+  name: "Linus Torvalds",
+  bio: "Linus Benedict Torvalds s a Finnish American software engineer, who was the principal driving force behind the development of the Linux kernel. Its creation itself is attributed towards him and he later became the chief architect of the Linux kernel, and is now the project’s coordinator.",
+  image: "http://www.arkhitech.com/wp-content/uploads/2015/04/Linus-Torvalds.jpg"
+  lifespan: {
+    birth: 1969,
+    death: 
+  }
+}
+
+{
+ title: "12 GREATEST PROGRAMMERS OF ALL TIME",
+  name: "Dennis Ritchie",
+  bio: "Dennis MacAlistair Ritchie was an American computer scientist who is credited for shaping and pioneering the digital era. He created the most commonly used C programming language that is used today in various software applications, embedded system development, operating systems, and has influenced most modern programming languages.",
+  image: "http://www.arkhitech.com/wp-content/uploads/2015/04/Dennis-Ritchie.jpg"
+  lifespan: {
+    birth: 1941,
+    death: 2011
+  }
+}
+
